df6e07a2c50e17d8ae31d798754eefe10bb30a40
browndw <davidwestbrown@gmail.com>
2023-02-11 17:36:07 -0500

    Update index.py

--- index.py
+++ index.py
@@ -21,13 +21,13 @@ PL_LOGO = str(HERE.joinpath("_static/porpoise_badge.svg"))
 UG_LOGO = str(HERE.joinpath("_static/user_guide.svg"))
 STYLE = str(HERE.joinpath("css/style.css"))
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data
 def get_img_with_header(local_img_path):
     img_format = os.path.splitext(local_img_path)[-1].replace(".", "")
     bin_str = get_base64_of_bin_file(local_img_path)
