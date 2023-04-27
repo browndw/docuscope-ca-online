03dc00d5eb0e1aa51055dfefa286fcfb312d682c
browndw <davidwestbrown@gmail.com>
2023-04-27 06:56:18 -0400

    Update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,2 +1,21 @@
-from server.downloads import download
-from server.session import session_state
+import pathlib
+import tomli
+
+HERE = pathlib.Path(__file__).parents[1].resolve()
+OPTIONS = str(HERE.joinpath("options.toml"))
+
+# import options
+with open(OPTIONS, mode="rb") as fp:
+	_options = tomli.load(fp)
+
+DESKTOP = _options['global']['desktop_mode']
+
+if DESKTOP == False:
+	from server.downloads import download
+	from server.session import session_state
+
+if DESKTOP == True:
+	from ..server.downloads import download
+	from ..server.session import session_state
+
+
