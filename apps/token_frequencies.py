652b85ec2027c41afffbf3db19b1c3eccfc3f297
browndw <davidwestbrown@gmail.com>
2023-08-18 14:17:48 -0400

    Update token_frequencies.py

--- apps/token_frequencies.py
+++ apps/token_frequencies.py
@@ -69,7 +69,7 @@ def main():
 		st.markdown(_messages.message_target_info(metadata_target))
 			
 		gb = st_aggrid.GridOptionsBuilder.from_dataframe(df)
-		gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=100) #Add pagination
+		gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=25) #Add pagination
 		gb.configure_column("Token", filter="agTextColumnFilter", headerCheckboxSelection = True, headerCheckboxSelectionFilteredOnly = True)
 		gb.configure_column("Tag", filter="agTextColumnFilter")
 		gb.configure_column("RF", type=["numericColumn","numberColumnFilter","customNumericFormat"], precision=2)
