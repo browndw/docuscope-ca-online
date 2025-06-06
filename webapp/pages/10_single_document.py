990a7a53cb1d30a3179c52952932c16ab05a889f
browndw <davidwestbrown@gmail.com>
2025-06-06 12:31:17 -0400

    Update 10_single_document.py

--- webapp/pages/10_single_document.py
+++ webapp/pages/10_single_document.py
@@ -48,6 +48,12 @@ def main():
     # Get or initialize user session
     user_session_id, session = _utils.handlers.get_or_init_user_session()
 
+    st.sidebar.link_button(
+        label="Help",
+        url="https://browndw.github.io/docuscope-docs/guide/single-document.html",
+        icon=":material/help:"
+        )
+
     if session.get('doc')[0] is True:
 
         _utils.handlers.load_widget_state(
