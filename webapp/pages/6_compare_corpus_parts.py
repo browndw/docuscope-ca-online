9dbc8123ebda948c0bbfc80e6af2f039bdc39d79
browndw <davidwestbrown@gmail.com>
2025-06-10 18:48:52 -0400

    Update 6_compare_corpus_parts.py

--- webapp/pages/6_compare_corpus_parts.py
+++ webapp/pages/6_compare_corpus_parts.py
@@ -23,8 +23,39 @@ project_root = pathlib.Path(__file__).parent.parents[1].resolve()
 if str(project_root) not in sys.path:
     sys.path.insert(0, str(project_root))
 
-import webapp.utilities as _utils   # noqa: E402
-from webapp.menu import menu, require_login   # noqa: E402
+from webapp.utilities.handlers import (  # noqa: E402
+    generate_keyness_parts,
+    get_or_init_user_session,
+    load_metadata,
+    update_session
+    )
+from webapp.utilities.ui import (   # noqa: E402
+    keyness_sort_controls,
+    load_widget_state,
+    persist,
+    reference_parts,
+    render_dataframe,
+    sidebar_action_button,
+    sidebar_help_link,
+    sidebar_keyness_options,
+    tag_filter_multiselect,
+    target_parts,
+    toggle_download,
+    update_ref,
+    update_tar
+    )
+from webapp.utilities.formatters import (  # noqa: E402
+    convert_to_excel,
+    plot_compare_corpus_bar,
+    plot_download_link
+    )
+from webapp.utilities.analysis import (   # noqa: E402
+    freq_simplify_pl
+    )
+from webapp.menu import (   # noqa: E402
+    menu,
+    require_login
+    )
 
 
 TITLE = "Compare Corpus Parts"
@@ -37,49 +68,58 @@ st.set_page_config(
 
 
 def main():
+    """
+    Main function to run the Streamlit app for comparing corpus parts.
+    This function sets up the page configuration, checks user login status,
+    initializes the user session, and renders the UI components for
+    comparing corpus parts based on selected categories.
+    It allows users to generate keyness tables, filter results by tags,
+    and download the data in Excel format.
+    """
     # Set login requirements for navigaton
     require_login()
     menu()
-    st.markdown(f"## {TITLE}")
-    # Get or initialize user session
-    user_session_id, session = _utils.handlers.get_or_init_user_session()
-
-    st.sidebar.link_button(
-        label="Help",
-        url="https://browndw.github.io/docuscope-docs/guide/compare-corpus-parts.html",
-        icon=":material/help:"
+    st.markdown(
+        body=f"## {TITLE}",
+        help=(
+            "This page allows you to compare different parts of your corpus "
+            "by generating a keyness table based on selected categories. "
+            "You can filter the results by tags and download the data in Excel format."
+            )
         )
+    # Get or initialize user session
+    user_session_id, session = get_or_init_user_session()
+
+    sidebar_help_link("compare-corpus-parts.html")
 
     if session.get('keyness_parts')[0] is True:
 
-        _utils.handlers.load_widget_state(
+        load_widget_state(
             pathlib.Path(__file__).stem,
             user_session_id
             )
-        metadata_target = _utils.handlers.load_metadata(
+        metadata_target = load_metadata(
             'target',
             user_session_id
             )
 
         col1, col2 = st.columns([1, 1])
         with col1:
-            st.info(
-                _utils.content.message_target_parts(
-                    metadata_target.get('keyness_parts')[0]['temp'])
-                )
+            st.info(target_parts(metadata_target.get('keyness_parts')[0]['temp']))
         with col2:
-            st.info(
-                _utils.content.message_reference_parts(
-                    metadata_target.get('keyness_parts')[0]['temp'])
-                    )
+            st.info(reference_parts(metadata_target.get('keyness_parts')[0]['temp']))
 
-        st.markdown("Showing keywords that reach significance at *p* < 0.01")
+        # --- Show user selections ---
+        st.info(
+            f"**p-value threshold:** {st.session_state[user_session_id]['pval_threshold']} &nbsp;&nbsp; "  # noqa: E501
+            f"**Swapped:** {'Yes' if st.session_state[user_session_id]['swap_target'] else 'No'}"  # noqa: E501
+        )
 
         st.sidebar.markdown("### Comparison")
         table_radio = st.sidebar.radio(
             "Select the keyness table to display:",
             ("Tokens", "Tags Only"),
-            key=_utils.handlers.persist(
+            key=persist(
                 "cp_radio1",
                 pathlib.Path(__file__).stem,
                 user_session_id),
@@ -93,7 +133,7 @@ def main():
                 "Select tags to display:",
                 ("Parts-of-Speech",
                  "DocuScope"),
-                key=_utils.handlers.persist(
+                key=persist(
                      "cp_radio2",
                      pathlib.Path(__file__).stem,
                      user_session_id),
@@ -108,75 +148,60 @@ def main():
                     )
                 if tag_type == 'General':
                     df = st.session_state[user_session_id]["target"]["kw_pos_cp"]
-                    df = _utils.analysis.freq_simplify_pl(df)
+                    df = freq_simplify_pl(df)
                 else:
                     df = st.session_state[user_session_id]["target"]["kw_pos_cp"]
             else:
                 df = st.session_state[user_session_id]["target"]["kw_ds_cp"]
 
-            if df.height == 0 or df is None:
-                cats = []
-            elif df.height > 0:
-                cats = sorted(df.get_column("Tag").unique().to_list())
+            sort_by, reverse = keyness_sort_controls(
+                sort_options=["Keyness (LL)", "Effect Size (LR)"],
+                default="Keyness (LL)",
+                reverse_default=True,
+                key_prefix="kt_"  # or something unique per page/tab
+            )
 
-            filter_vals = st.multiselect("Select tags to filter:", (cats))
-            if len(filter_vals) > 0:
-                df = df.filter(pl.col("Tag").is_in(filter_vals))
+            df = tag_filter_multiselect(df)
 
-            st.dataframe(
-                df,
-                hide_index=True,
-                column_config=_utils.formatters.get_streamlit_column_config(df)
-                )
+            # Map UI label to actual DataFrame column
+            sort_col_map = {
+                "Keyness (LL)": "LL",
+                "Effect Size (LR)": "LR"
+            }
+            sort_col = sort_col_map[sort_by]
 
-            with st.expander("Column explanation"):
-                st.markdown(_utils.content.message_columns_keyness)
+            if df is not None and getattr(df, "height", 0) > 0:
+                df = df.sort(sort_col, descending=reverse)
+
+            render_dataframe(df)
 
             st.sidebar.markdown("---")
 
-            _utils.formatters.toggle_download(
+            toggle_download(
                 label="Excel",
-                convert_func=_utils.formatters.convert_to_excel,
-                convert_args=(df.to_pandas(),),
+                convert_func=convert_to_excel,
+                convert_args=(df.to_pandas(),) if (df is not None and getattr(df, "height", 0) > 0) else (None,),  # noqa: E501
                 file_name="keywords_tokens.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                message=_utils.content.message_download,
                 location=st.sidebar
             )
 
             st.sidebar.markdown("---")
 
-            st.sidebar.markdown("### Generate new table")
-            st.sidebar.markdown("""
-                            Click the button to reset the keyness table.
-                            """)
+            st.sidebar.markdown(
+                body=(
+                    "### Generate new table\n\n"
+                    "Use the button to reset the keyness table and start over."
+                    )
+                )
 
             if st.sidebar.button(
-                label="Compare New Categories",
-                icon=":material/refresh:",
-                ):
-                if "kw_pos_cp" not in st.session_state[user_session_id]["target"]:  # noqa: E501
-                    st.session_state[user_session_id]["target"]["kw_pos_cp"] = {}
-                st.session_state[user_session_id]["target"]["kw_pos_cp"] = {}
-
-                if "kw_ds_cp" not in st.session_state[user_session_id]["target"]:  # noqa: E501
-                    st.session_state[user_session_id]["target"]["kw_ds_cp"] = {}
-                st.session_state[user_session_id]["target"]["kw_ds_cp"] = {}
-
-                if "kt_pos_cp" not in st.session_state[user_session_id]["target"]:  # noqa: E501
-                    st.session_state[user_session_id]["target"]["kt_pos_cp"] = {}
-                st.session_state[user_session_id]["target"]["kt_pos_cp"] = {}
-
-                if "kt_ds_cp" not in st.session_state[user_session_id]["target"]:  # noqa: E501
-                    st.session_state[user_session_id]["target"]["kt_ds_cp"] = {}
-                st.session_state[user_session_id]["target"]["kt_ds_cp"] = {}
-
-                _utils.handlers.update_session(
-                    'keyness_parts',
-                    False,
-                    user_session_id
-                    )
-
+                    label="Compare New Categories",
+                    icon=":material/refresh:",
+            ):
+                for key in ["kw_pos_cp", "kw_ds_cp", "kt_pos_cp", "kt_ds_cp"]:
+                    st.session_state[user_session_id]["target"][key] = {}
+                update_session('keyness_parts', False, user_session_id)
                 st.rerun()
 
             st.sidebar.markdown("---")
@@ -188,7 +213,7 @@ def main():
                 "Select tags to display:",
                 ("Parts-of-Speech",
                  "DocuScope"),
-                key=_utils.handlers.persist(
+                key=persist(
                     "cp_radio3", pathlib.Path(__file__).stem, user_session_id
                     ),
                 horizontal=True
@@ -208,87 +233,74 @@ def main():
 
             tab1, tab2 = st.tabs(["Keyness Table", "Keyness Plot"])
             with tab1:
-                if df.height == 0 or df is None:
-                    cats = []
-                elif df.height > 0:
-                    cats = sorted(df.get_column("Tag").unique().to_list())
-
-                filter_vals = st.multiselect("Select tags to filter:", (cats))
-                if len(filter_vals) > 0:
-                    df = df.filter(pl.col("Tag").is_in(filter_vals))
-
-                st.dataframe(
-                    df,
-                    hide_index=True,
-                    column_config=_utils.formatters.get_streamlit_column_config(df)
-                    )
+                df = tag_filter_multiselect(df)
+                render_dataframe(df)
 
             with tab2:
                 if df.height > 0:
-                    fig = _utils.formatters.plot_compare_corpus_bar(df)
+                    fig = plot_compare_corpus_bar(df)
                     st.plotly_chart(fig, use_container_width=True)
-
-            with st.expander("Column explanation"):
-                st.markdown(_utils.content.message_columns_keyness)
+                    plot_download_link(fig, filename="compare_corpus_bar.png")
 
             st.sidebar.markdown("---")
 
-            _utils.formatters.toggle_download(
+            toggle_download(
                 label="Excel",
-                convert_func=_utils.formatters.convert_to_excel,
-                convert_args=(df.to_pandas(),),
+                convert_func=convert_to_excel,
+                convert_args=(df.to_pandas(),) if (df is not None and getattr(df, "height", 0) > 0) else (None,),  # noqa: E501
                 file_name="keywords_tags.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                message=_utils.content.message_download,
                 location=st.sidebar
             )
 
             st.sidebar.markdown("---")
 
-            st.sidebar.markdown(_utils.content.message_reset_table)
+            st.sidebar.markdown(
+                body=(
+                    "### Generate new table\n\n"
+                    "Use the button to reset the keyness table and start over."
+                    )
+                )
 
             if st.sidebar.button("Compare New Categories", icon=":material/refresh:"):
                 for key in ["kw_pos_cp", "kw_ds_cp", "kt_pos_cp", "kt_ds_cp"]:
                     st.session_state[user_session_id]["target"][key] = {}
-                _utils.handlers.update_session('keyness_parts', False, user_session_id)
+                update_session('keyness_parts', False, user_session_id)
                 st.rerun()
 
             st.sidebar.markdown("---")
 
     else:
-
         st.markdown(
-            """
-            ###### :material/manufacturing: \
-            Use the options in the sidebar to generate a table from subcorpora.\n
+            body=(
+                ":material/manufacturing: Use the button in the sidebar to **generate keywords** from subcorpora.\n\n"  # noqa: E501
+                ":material/priority: To use this tool, you must first process **metadata** from **Manage Corpus Data**.\n\n"  # noqa: E501
+                ":material/priority: After the table has been generated, "
+                "you will be able to **toggle between the tagsets**."
+                )
+        )
 
-            * To use this tool, you must first process **metadata**
-            from your file names. This can be done from **Manage Corpus Data**.
-
-            * Categories of interest can be placed at the beginning
-            of file names before an underscore:
-            ```
-            BIO_G0_02_1.txt, BIO_G0_03_1.txt, ENG_G0_16_1.txt, ENG_G0_21_1.txt, HIS_G0_02_1.txt, HIS_G0_03_1.txt
-            ```
-            * Processing these names would yield the categories:
-            ```
-            BIO, ENG, HIS
-            ```
-            * Those categories could then be compared in any combination.\n
-
-            :material/block: Selecting of the same category as target
-            and reference is prevented.
-            """  # noqa: E501
+        st.sidebar.markdown(
+            body="### Select categories to compare",
+            help=(
+                "Categories can be generated from file names in the target corpus.\n\n"
+                "For example, if your file names are formatted like `BIO_G0_02_1.txt`, "
+                "`ENG_G0_16_1.txt`, etc., you can extract the categories `BIO` and `ENG`. "  # noqa: E501
+                "These categories can then be selected for comparison in the keyness table.\n\n"  # noqa: E501
+                "You can select multiple categories for both target and reference corpora, "
+                "but you cannot select the same category for both target and reference."
+                "If you have not yet processed metadata, please do so in the **Manage Corpus Data** app."  # noqa: E501
+            )
+            )
+        st.sidebar.markdown(
+            body=(
+                "After **target** and **reference** categories have been selected, "
+                "click the button to generate a keyness table."
+                )
             )
 
-        st.sidebar.markdown("### Select categories to compare")
-        st.sidebar.markdown("""After **target** and **reference** categories
-                            have been selected,
-                            click the button to generate a keyness table.
-                            """)
-
         if session.get('has_meta')[0] is True:
-            metadata_target = _utils.handlers.load_metadata(
+            metadata_target = load_metadata(
                 'target',
                 user_session_id
             )
@@ -296,7 +308,7 @@ def main():
             st.session_state[user_session_id]['tar'] = st.sidebar.multiselect(
                 "Select target categories:",
                 (sorted(set(metadata_target.get('doccats')[0]['cats']))),
-                _utils.handlers.update_tar(user_session_id),
+                update_tar(user_session_id),
                 key=f"tar_{user_session_id}"
             )
 
@@ -308,7 +320,7 @@ def main():
                 )
 
         if session.get('has_meta')[0] is True:
-            metadata_target = _utils.handlers.load_metadata(
+            metadata_target = load_metadata(
                 'target',
                 user_session_id
                 )
@@ -317,7 +329,7 @@ def main():
             st.session_state[user_session_id]['ref'] = st.sidebar.multiselect(
                 "Select reference categories:",
                 (sorted(set(metadata_target.get('doccats')[0]['cats']))),
-                _utils.handlers.update_ref(user_session_id),
+                update_ref(user_session_id),
                 key=f"ref_{user_session_id}"
                 )
 
@@ -330,22 +342,43 @@ def main():
 
         st.sidebar.markdown("---")
 
-        st.sidebar.markdown(_utils.content.message_generate_table)
+        pval_selected, swap_selected = sidebar_keyness_options(
+            user_session_id,
+            load_metadata_func=load_metadata
+        )
+        # Display the sidebar header for generating frequency table
+        st.sidebar.markdown(
+            body=(
+                "### Generate table\n\n"
+                "Use the button to process a table."
+                ),
+            help=(
+                "Tables are generated based on the target and reference corpora. "
+                "You can filter the table after it has been generated. "
+                "The table will include frequencies and hypothesis testing for the selected tagsets.\n\n"  # noqa: E501
+                "Click on the **Help** button for more information on how to use this app."  # noqa: E501
+                )
+            )
 
-        _utils.handlers.sidebar_action_button(
+        sidebar_action_button(
             button_label="Keyness Table of Corpus Parts",
             button_icon=":material/manufacturing:",
             preconditions=[
                 session.get('has_target')[0],
             ],
-            action=lambda: _utils.handlers.generate_keyness_parts(user_session_id),
+            action=lambda: generate_keyness_parts(
+                user_session_id,
+                threshold=pval_selected,
+                swap_target=swap_selected
+                ),
             spinner_message="Generating keywords..."
         )
 
         if st.session_state[user_session_id].get("keyness_parts_warning"):
             msg, icon = st.session_state[user_session_id]["keyness_parts_warning"]
-            st.warning(msg, icon=icon)
-
+            st.error(msg, icon=icon)
+            # Clear the warning after displaying it
+            del st.session_state[user_session_id]["keyness_parts_warning"]
         st.sidebar.markdown("---")
 
 
