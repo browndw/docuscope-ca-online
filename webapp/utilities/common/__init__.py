245db703298b749a9862236d52ed3a9f127fe4c4
browndw <davidwestbrown@gmail.com>
2025-10-19 10:25:05 -0400

    Update __init__.py

--- webapp/utilities/common/__init__.py
+++ webapp/utilities/common/__init__.py
@@ -5,8 +5,9 @@ This package provides utility functions for working with documents, IDs,
 and basic data structures without complex dependencies.
 """
 
-from webapp.utilities.common.document_utils import get_doc_cats
+from webapp.utilities.common.document_utils import get_doc_cats, safe_metadata_get
 
 __all__ = [
-    'get_doc_cats'
+    'get_doc_cats',
+    'safe_metadata_get'
 ]
